85a88839497c696f03f7b7022d536c23d1c7580a
alicia263 <aliciaaaron263@gmail.com>
2024-10-07 11:42:17 +0200

    Update main.tf

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -11,6 +11,8 @@ provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  key_name=var.key_name
+  
 }
 
 # Define variables for AWS credentials and other configurations
