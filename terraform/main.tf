9e500cd6434d94493157d7286adf9981c16c25fb
alicia263 <aliciaaaron263@gmail.com>
2024-10-07 15:30:02 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "econet-chatbot-terraform-state"  # Replace with your S3 bucket name
+    key            = "terraform/state"         # The path within the bucket to store the state
+    region         = "us-east-1"               # The AWS region of the bucket
+    dynamodb_table = "terraform-locks"         # Optional: Lock table for state locking (see below)
+  }
 }
 
 provider "aws" {
@@ -13,21 +20,17 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
   
-
-
-# Define variables for AWS credentials and other configurationss
+# Define variables for AWS credentials and other configurations
 variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
   sensitive   = true
 }
-
 variable "aws_secret_key" {
   description = "AWS Secret Key"
   type        = string
   sensitive   = true
 }
-
 variable "key_name" {
   description = "Name of the SSH key pair"
   type        = string
@@ -51,42 +54,37 @@ resource "local_file" "private_key" {
 resource "aws_security_group" "sg_ec2" {
   name        = "sg_ec2_airflow"
   description = "Security group for EC2 running Airflow and related services"
-
+  
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 9200
     to_port     = 9200
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 8001
     to_port     = 8001
@@ -107,16 +105,16 @@ resource "aws_instance" "airflow_instance" {
   instance_type = "t2.xlarge"  # Increased instance size due to resource requirements
   key_name      = aws_key_pair.key_pair.key_name
   vpc_security_group_ids = [aws_security_group.sg_ec2.id]
-
+  
   tags = {
     Name = "airflow_instance"
   }
-
+  
   root_block_device {
-    volume_size = 30
+    volume_size = 60
     volume_type = "gp2"
   }
-
+  
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
